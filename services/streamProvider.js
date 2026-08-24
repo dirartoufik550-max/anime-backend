a077af1427d47f164f6e2445388c82f7aceaf86f
dirartoufik550-max <dirartoufik550@gmail.com>
2026-08-24 19:52:10 +0100

    Update streamProvider.js

--- services/streamProvider.js
+++ services/streamProvider.js
@@ -1,19 +1,11 @@
 async function getAnimeEpisodeStream(animeTitle, episodeNum = 1) {
-  try {
-    // رابط الفيديو المباشر من سيرفر SoraPlay
-    const directMp4Url = "https://soraplay.xyz/FibCU10knKEu8/0708b953cfaaa48084c05e46b3b87931/%5BWitanime.com%5D+JK+EP+01+BD-FHD-480p.mp4";
+  // رابط الفيديو المباشر المستخرج من SoraPlay
+  const directMp4Url = "https://soraplay.xyz/FibCU10knKEu8/0708b953cfaaa48084c05e46b3b87931/%5BWitanime.com%5D+JK+EP+01+BD-FHD-480p.mp4";
 
-    return {
-      streamUrl: directMp4Url,
-      isHLS: false
-    };
-  } catch (error) {
-    console.error('Stream Provider Error:', error.message);
-    return {
-      streamUrl: "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
-      isHLS: false
-    };
-  }
+  return {
+    streamUrl: directMp4Url,
+    isHLS: false
+  };
 }
 
 module.exports = { getAnimeEpisodeStream };
